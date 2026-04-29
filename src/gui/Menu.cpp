161f89264fec08fb45cac96a5d1e58d447b97905
MustyKrab <jarrodmorris5@gmail.com>
2026-04-29 19:20:50 -0400

    Update Menu to use thread-safe XRay toggle

--- src/gui/Menu.cpp
+++ src/gui/Menu.cpp
@@ -1,12 +1,12 @@
 #include "Menu.h"
 #include "../modules/KillAura.h"
 #include "../modules/TeleportAura.h"
+#include "../modules/XRay.h"
 #include <imgui.h>
 
 void Menu::RenderCombatTab() {
     ImGui::BeginChild("Combat", ImVec2(0, 0), true);
     
-    // Separate toggles in the UI
     ImGui::Checkbox("KillAura", &Modules::KillAura::bEnabled);
     if (ImGui::IsItemHovered())
         ImGui::SetTooltip("Standard melee aura. Keep reach under 3.0f for bypass.");
@@ -14,6 +14,17 @@ void Menu::RenderCombatTab() {
     ImGui::Checkbox("Teleport Aura", &Modules::TeleportAura::bEnabled);
     if (ImGui::IsItemHovered())
         ImGui::SetTooltip("Cross-map teleport attacks. Disable KillAura before using.");
+
+    ImGui::EndChild();
+}
+
+void Menu::RenderVisualsTab() {
+    ImGui::BeginChild("Visuals", ImVec2(0, 0), true);
+    
+    bool xrayState = Modules::XRay::bEnabled;
+    if (ImGui::Checkbox("XRay", &xrayState)) {
+        Modules::XRay::Toggle(); // Safely queues the update
+    }
 
     ImGui::EndChild();
 }
