648de388b5217f641054988235771ce35e1b9a8e
MustyKrab <jarrodmorris5@gmail.com>
2026-04-28 21:25:55 -0400

    Fix obfuscated class signatures to match Lunar Client 1.21.11

--- src/modules/render/ESP.cpp
+++ src/modules/render/ESP.cpp
@@ -156,6 +156,14 @@ void ESP::DrawSkeleton(Graphics& g, Vec3 feet, float bodyYaw, float height, Vec3
     }
 }
 
+jclass FindClassSafe(JNIEnv* env, const char* sig, const char* fallbackName) {
+    jclass cls = JNIHelper::FindClassBySignature(sig);
+    if (!cls) {
+        cls = env->FindClass(fallbackName);
+    }
+    return cls;
+}
+
 void ESP::RenderLoop() {
     GdiplusStartupInput gdiplusStartupInput;
     GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
@@ -183,43 +191,77 @@ void ESP::RenderLoop() {
     JNIEnv* env = JNIHelper::env;
     if (!env) return;
 
-    // Cache classes
-    jclass mcClass = JNIHelper::FindClassBySignature("Lnet/minecraft/class_310;");
-    jclass worldClass = JNIHelper::FindClassBySignature("Lnet/minecraft/class_638;");
-    jclass entityClass = JNIHelper::FindClassBySignature("Lnet/minecraft/class_1297;");
-    jclass livingClass = JNIHelper::FindClassBySignature("Lnet/minecraft/class_1309;");
-    jclass rendererClass = JNIHelper::FindClassBySignature("Lnet/minecraft/class_757;");
-    jclass cameraClass = JNIHelper::FindClassBySignature("Lnet/minecraft/class_4184;");
-    jclass vec3dClass = JNIHelper::FindClassBySignature("Lnet/minecraft/class_243;");
-    jclass matrixClass = JNIHelper::FindClassBySignature("Lorg/joml/Matrix4f;");
+    // Retry loop for resolving classes (Minecraft might still be loading)
+    jclass mcClass = nullptr, worldClass = nullptr, entityClass = nullptr, livingClass = nullptr;
+    jclass rendererClass = nullptr, cameraClass = nullptr, vec3dClass = nullptr, matrixClass = nullptr;
+    
+    int retries = 0;
+    while (running && retries < 30) {
+        mcClass = FindClassSafe(env, "Lnet/minecraft/class_310;", "net/minecraft/client/MinecraftClient");
+        worldClass = FindClassSafe(env, "Lnet/minecraft/class_638;", "net/minecraft/client/world/ClientWorld");
+        entityClass = FindClassSafe(env, "Lnet/minecraft/class_1297;", "net/minecraft/entity/Entity");
+        livingClass = FindClassSafe(env, "Lnet/minecraft/class_1309;", "net/minecraft/entity/LivingEntity");
+        rendererClass = FindClassSafe(env, "Lnet/minecraft/class_757;", "net/minecraft/client/render/GameRenderer");
+        cameraClass = FindClassSafe(env, "Lnet/minecraft/class_4184;", "net/minecraft/client/render/Camera");
+        vec3dClass = FindClassSafe(env, "Lnet/minecraft/class_243;", "net/minecraft/util/math/Vec3d");
+        matrixClass = FindClassSafe(env, "Lorg/joml/Matrix4f;", "org/joml/Matrix4f");
 
-    if (!mcClass || !worldClass || !entityClass || !livingClass || !rendererClass || !cameraClass || !vec3dClass || !matrixClass) {
-        std::cout << "[MustyClient] Failed to resolve obfuscated classes." << std::endl;
-        return;
+        if (mcClass && worldClass && entityClass && livingClass && rendererClass && cameraClass && vec3dClass && matrixClass) {
+            break;
+        }
+        
+        std::cout << "[MustyClient] Waiting for classes to load... (" << retries << "/30)" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        retries++;
     }
 
+    if (!mcClass || !worldClass || !entityClass || !livingClass || !rendererClass || !cameraClass || !vec3dClass || !matrixClass) {
+        std::cout << "[MustyClient] Failed to resolve obfuscated classes after 30 seconds." << std::endl;
+        return;
+    }
+    
+    std::cout << "[MustyClient] Classes resolved successfully." << std::endl;
+
     jfieldID instanceField = env->GetStaticFieldID(mcClass, "field_1700", "Lnet/minecraft/class_310;");
+    if (!instanceField) instanceField = env->GetStaticFieldID(mcClass, "instance", "Lnet/minecraft/client/MinecraftClient;");
+
     jfieldID localPlayerField = env->GetFieldID(mcClass, "field_1724", "Lnet/minecraft/class_746;");
+    if (!localPlayerField) localPlayerField = env->GetFieldID(mcClass, "player", "Lnet/minecraft/client/network/ClientPlayerEntity;");
+
     jfieldID worldField = env->GetFieldID(mcClass, "field_1687", "Lnet/minecraft/class_638;");
+    if (!worldField) worldField = env->GetFieldID(mcClass, "world", "Lnet/minecraft/client/world/ClientWorld;");
+
     jfieldID rendererField = env->GetFieldID(mcClass, "field_1773", "Lnet/minecraft/class_757;");
+    if (!rendererField) rendererField = env->GetFieldID(mcClass, "gameRenderer", "Lnet/minecraft/client/render/GameRenderer;");
 
     jfieldID playersField = env->GetFieldID(worldClass, "field_18226", "Ljava/util/List;");
+    if (!playersField) playersField = env->GetFieldID(worldClass, "players", "Ljava/util/List;");
+
     jclass listClass = env->FindClass("java/util/List");
     jmethodID listSize = env->GetMethodID(listClass, "size", "()I");
     jmethodID listGet = env->GetMethodID(listClass, "get", "(I)Ljava/lang/Object;");
 
     jfieldID entX = env->GetFieldID(entityClass, "field_6014", "D");
+    if (!entX) entX = env->GetFieldID(entityClass, "x", "D");
     jfieldID entY = env->GetFieldID(entityClass, "field_6036", "D");
+    if (!entY) entY = env->GetFieldID(entityClass, "y", "D");
     jfieldID entZ = env->GetFieldID(entityClass, "field_5969", "D");
+    if (!entZ) entZ = env->GetFieldID(entityClass, "z", "D");
     
     jmethodID getHealth = env->GetMethodID(livingClass, "method_6032", "()F");
+    if (!getHealth) getHealth = env->GetMethodID(livingClass, "getHealth", "()F");
     jmethodID getMaxHealth = env->GetMethodID(livingClass, "method_6063", "()F");
-    jfieldID bodyYaw = env->GetFieldID(livingClass, "field_6283", "F"); // bodyYaw
+    if (!getMaxHealth) getMaxHealth = env->GetMethodID(livingClass, "getMaxHealth", "()F");
+    jfieldID bodyYaw = env->GetFieldID(livingClass, "field_6283", "F");
+    if (!bodyYaw) bodyYaw = env->GetFieldID(livingClass, "bodyYaw", "F");
 
     jfieldID camField = env->GetFieldID(rendererClass, "lunar$savedCamera", "Lnet/minecraft/class_4184;");
+    if (!camField) camField = env->GetFieldID(rendererClass, "camera", "Lnet/minecraft/client/render/Camera;");
+    
     jfieldID modelViewField = env->GetFieldID(rendererClass, "lunar$savedModelView$v1_19_3", "Lorg/joml/Matrix4f;");
     jfieldID projField = env->GetFieldID(rendererClass, "lunar$savedProjection$v1_19_3", "Lorg/joml/Matrix4f;");
     jfieldID camPosField = env->GetFieldID(cameraClass, "field_18712", "Lnet/minecraft/class_243;");
+    if (!camPosField) camPosField = env->GetFieldID(cameraClass, "pos", "Lnet/minecraft/util/math/Vec3d;");
 
     const char* mNames[] = { "m00","m01","m02","m03", "m10","m11","m12","m13", "m20","m21","m22","m23", "m30","m31","m32","m33" };
     jfieldID matrixFields[16];
@@ -252,110 +294,109 @@ void ESP::RenderLoop() {
         g.SetSmoothingMode(SmoothingModeAntiAlias);
         g.SetTextRenderingHint(TextRenderingHintAntiAlias);
 
-        jobject mcInstance = env->GetStaticObjectField(mcClass, instanceField);
-        if (mcInstance) {
-            jobject localPlayer = env->GetObjectField(mcInstance, localPlayerField);
-            jobject renderer = env->GetObjectField(mcInstance, rendererField);
-            jobject world = env->GetObjectField(mcInstance, worldField);
+        if (instanceField) {
+            jobject mcInstance = env->GetStaticObjectField(mcClass, instanceField);
+            if (mcInstance) {
+                jobject localPlayer = localPlayerField ? env->GetObjectField(mcInstance, localPlayerField) : nullptr;
+                jobject renderer = rendererField ? env->GetObjectField(mcInstance, rendererField) : nullptr;
+                jobject world = worldField ? env->GetObjectField(mcInstance, worldField) : nullptr;
 
-            if (renderer && world) {
-                jobject camera = env->GetObjectField(renderer, camField);
-                jobject modelViewObj = env->GetObjectField(renderer, modelViewField);
-                jobject projObj = env->GetObjectField(renderer, projField);
-                jobject playersList = env->GetObjectField(world, playersField);
+                if (renderer && world) {
+                    jobject camera = camField ? env->GetObjectField(renderer, camField) : nullptr;
+                    jobject modelViewObj = modelViewField ? env->GetObjectField(renderer, modelViewField) : nullptr;
+                    jobject projObj = projField ? env->GetObjectField(renderer, projField) : nullptr;
+                    jobject playersList = playersField ? env->GetObjectField(world, playersField) : nullptr;
 
-                if (camera && modelViewObj && projObj && playersList) {
-                    jobject camPosObj = env->GetObjectField(camera, camPosField);
-                    
-                    // Fetch Camera Pos
-                    jint fCount; jfieldID* fds;
-                    JNIHelper::jvmti->GetClassFields(vec3dClass, &fCount, &fds);
-                    jfieldID vecFields[3]; int vIdx = 0;
-                    for (int i = 0; i < fCount; i++) {
-                        char* sig; JNIHelper::jvmti->GetFieldName(vec3dClass, fds[i], nullptr, &sig, nullptr);
-                        if (strcmp(sig, "D") == 0 && vIdx < 3) vecFields[vIdx++] = fds[i];
-                        JNIHelper::jvmti->Deallocate((unsigned char*)sig);
-                    }
-                    JNIHelper::jvmti->Deallocate((unsigned char*)fds);
-
-                    Vec3 camPos = {
-                        env->GetDoubleField(camPosObj, vecFields[0]),
-                        env->GetDoubleField(camPosObj, vecFields[1]),
-                        env->GetDoubleField(camPosObj, vecFields[2])
-                    };
-                    env->DeleteLocalRef(camPosObj);
-
-                    float mv[16], p[16];
-                    for (int i = 0; i < 16; i++) {
-                        mv[i] = env->GetFloatField(modelViewObj, matrixFields[i]);
-                        p[i] = env->GetFloatField(projObj, matrixFields[i]);
-                    }
-
-                    jint size = env->CallIntMethod(playersList, listSize);
-                    for (int i = 0; i < size; i++) {
-                        jobject player = env->CallObjectMethod(playersList, listGet, i);
-                        if (!player) continue;
-
-                        if (localPlayer && env->IsSameObject(player, localPlayer)) {
-                            env->DeleteLocalRef(player);
-                            continue;
-                        }
-
-                        Vec3 feetPos = {
-                            env->GetDoubleField(player, entX),
-                            env->GetDoubleField(player, entY),
-                            env->GetDoubleField(player, entZ)
-                        };
-
-                        Vec3 headPos = feetPos;
-                        headPos.y += 2.0; // Approx height
-
-                        Vec2 screenFeet, screenHead;
-                        if (WorldToScreen(feetPos, camPos, mv, p, screenFeet, width, height) &&
-                            WorldToScreen(headPos, camPos, mv, p, screenHead, width, height)) {
-
-                            float boxHeight = screenFeet.y - screenHead.y;
-                            float boxWidth = boxHeight / 2.0f;
-                            float boxX = screenHead.x - boxWidth / 2.0f;
-                            float boxY = screenHead.y;
-
-                            // 1. Corner Box
-                            DrawCornerBox(g, boxX, boxY, boxWidth, boxHeight, Color(255, 255, 255, 255));
-
-                            // 2. Health Bar
-                            float hp = 20.0f, maxHp = 20.0f;
-                            if (getHealth && getMaxHealth) {
-                                hp = env->CallFloatMethod(player, getHealth);
-                                maxHp = env->CallFloatMethod(player, getMaxHealth);
+                    if (camera && modelViewObj && projObj && playersList) {
+                        jobject camPosObj = camPosField ? env->GetObjectField(camera, camPosField) : nullptr;
+                        
+                        if (camPosObj) {
+                            jint fCount; jfieldID* fds;
+                            JNIHelper::jvmti->GetClassFields(vec3dClass, &fCount, &fds);
+                            jfieldID vecFields[3]; int vIdx = 0;
+                            for (int i = 0; i < fCount; i++) {
+                                char* sig; JNIHelper::jvmti->GetFieldName(vec3dClass, fds[i], nullptr, &sig, nullptr);
+                                if (strcmp(sig, "D") == 0 && vIdx < 3) vecFields[vIdx++] = fds[i];
+                                JNIHelper::jvmti->Deallocate((unsigned char*)sig);
                             }
-                            DrawHealthBar(g, boxX - 6.0f, boxY, 3.0f, boxHeight, hp, maxHp);
+                            JNIHelper::jvmti->Deallocate((unsigned char*)fds);
 
-                            // 3. Skeleton
-                            float bYaw = 0.0f;
-                            if (bodyYaw) bYaw = env->GetFloatField(player, bodyYaw);
-                            DrawSkeleton(g, feetPos, bYaw, 1.8f, camPos, mv, p, width, height);
+                            Vec3 camPos = {
+                                env->GetDoubleField(camPosObj, vecFields[0]),
+                                env->GetDoubleField(camPosObj, vecFields[1]),
+                                env->GetDoubleField(camPosObj, vecFields[2])
+                            };
+                            env->DeleteLocalRef(camPosObj);
 
-                            // 4. Nametag & Distance
-                            double distance = std::sqrt(std::pow(camPos.x - feetPos.x, 2) + std::pow(camPos.y - feetPos.y, 2) + std::pow(camPos.z - feetPos.z, 2));
-                            wchar_t textBuf[256];
-                            swprintf_s(textBuf, L"Player [%.1fm] \u2764%.1f", distance, hp);
-                            
-                            PointF textPos(screenHead.x, boxY - 16.0f);
-                            g.DrawString(textBuf, -1, &font, PointF(textPos.X + 1, textPos.Y + 1), &format, &shadowBrush);
-                            g.DrawString(textBuf, -1, &font, textPos, &format, &textBrush);
+                            float mv[16], p[16];
+                            for (int i = 0; i < 16; i++) {
+                                mv[i] = env->GetFloatField(modelViewObj, matrixFields[i]);
+                                p[i] = env->GetFloatField(projObj, matrixFields[i]);
+                            }
+
+                            jint size = env->CallIntMethod(playersList, listSize);
+                            for (int i = 0; i < size; i++) {
+                                jobject player = env->CallObjectMethod(playersList, listGet, i);
+                                if (!player) continue;
+
+                                if (localPlayer && env->IsSameObject(player, localPlayer)) {
+                                    env->DeleteLocalRef(player);
+                                    continue;
+                                }
+
+                                Vec3 feetPos = {
+                                    env->GetDoubleField(player, entX),
+                                    env->GetDoubleField(player, entY),
+                                    env->GetDoubleField(player, entZ)
+                                };
+
+                                Vec3 headPos = feetPos;
+                                headPos.y += 2.0; // Approx height
+
+                                Vec2 screenFeet, screenHead;
+                                if (WorldToScreen(feetPos, camPos, mv, p, screenFeet, width, height) &&
+                                    WorldToScreen(headPos, camPos, mv, p, screenHead, width, height)) {
+
+                                    float boxHeight = screenFeet.y - screenHead.y;
+                                    float boxWidth = boxHeight / 2.0f;
+                                    float boxX = screenHead.x - boxWidth / 2.0f;
+                                    float boxY = screenHead.y;
+
+                                    DrawCornerBox(g, boxX, boxY, boxWidth, boxHeight, Color(255, 255, 255, 255));
+
+                                    float hp = 20.0f, maxHp = 20.0f;
+                                    if (getHealth && getMaxHealth) {
+                                        hp = env->CallFloatMethod(player, getHealth);
+                                        maxHp = env->CallFloatMethod(player, getMaxHealth);
+                                    }
+                                    DrawHealthBar(g, boxX - 6.0f, boxY, 3.0f, boxHeight, hp, maxHp);
+
+                                    float bYaw = 0.0f;
+                                    if (bodyYaw) bYaw = env->GetFloatField(player, bodyYaw);
+                                    DrawSkeleton(g, feetPos, bYaw, 1.8f, camPos, mv, p, width, height);
+
+                                    double distance = std::sqrt(std::pow(camPos.x - feetPos.x, 2) + std::pow(camPos.y - feetPos.y, 2) + std::pow(camPos.z - feetPos.z, 2));
+                                    wchar_t textBuf[256];
+                                    swprintf_s(textBuf, L"Player [%.1fm] \u2764%.1f", distance, hp);
+                                    
+                                    PointF textPos(screenHead.x, boxY - 16.0f);
+                                    g.DrawString(textBuf, -1, &font, PointF(textPos.X + 1, textPos.Y + 1), &format, &shadowBrush);
+                                    g.DrawString(textBuf, -1, &font, textPos, &format, &textBrush);
+                                }
+                                env->DeleteLocalRef(player);
+                            }
                         }
-                        env->DeleteLocalRef(player);
+                        if (camera) env->DeleteLocalRef(camera);
+                        if (modelViewObj) env->DeleteLocalRef(modelViewObj);
+                        if (projObj) env->DeleteLocalRef(projObj);
+                        if (playersList) env->DeleteLocalRef(playersList);
                     }
                 }
-                if (camera) env->DeleteLocalRef(camera);
-                if (modelViewObj) env->DeleteLocalRef(modelViewObj);
-                if (projObj) env->DeleteLocalRef(projObj);
-                if (playersList) env->DeleteLocalRef(playersList);
+                if (renderer) env->DeleteLocalRef(renderer);
+                if (world) env->DeleteLocalRef(world);
+                if (localPlayer) env->DeleteLocalRef(localPlayer);
+                env->DeleteLocalRef(mcInstance);
             }
-            if (renderer) env->DeleteLocalRef(renderer);
-            if (world) env->DeleteLocalRef(world);
-            if (localPlayer) env->DeleteLocalRef(localPlayer);
-            env->DeleteLocalRef(mcInstance);
         }
 
         BitBlt(hdc, 0, 0, width, height, memDC, 0, 0, SRCCOPY);
