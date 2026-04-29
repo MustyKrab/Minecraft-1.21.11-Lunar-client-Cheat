bb8d4c9e59299c209c99bc0b9811c9e82eddc28b
MustyKrab <jarrodmorris5@gmail.com>
2026-04-29 19:52:17 -0400

    Update Menu.cpp to separate TeleportAura into its own tab

--- src/gui/Menu.cpp
+++ src/gui/Menu.cpp
@@ -1,19 +1,26 @@
 #include "Menu.h"
-#include "../modules/KillAura.h"
+#include "../modules/combat/Killaura.h"
 #include "../modules/TeleportAura.h"
-#include "../modules/XRay.h"
+#include "../modules/render/XRay.h"
 #include <imgui.h>
 
 void Menu::RenderCombatTab() {
     ImGui::BeginChild("Combat", ImVec2(0, 0), true);
     
+    // Killaura is now standalone
     ImGui::Checkbox("KillAura", &Modules::KillAura::bEnabled);
     if (ImGui::IsItemHovered())
         ImGui::SetTooltip("Standard melee aura. Keep reach under 3.0f for bypass.");
 
+    ImGui::EndChild();
+}
+
+void Menu::RenderTeleportAuraTab() {
+    ImGui::BeginChild("Teleport Aura", ImVec2(0, 0), true);
+    
     ImGui::Checkbox("Teleport Aura", &Modules::TeleportAura::bEnabled);
     if (ImGui::IsItemHovered())
-        ImGui::SetTooltip("Cross-map teleport attacks. Disable KillAura before using.");
+        ImGui::SetTooltip("Cross-map teleport attacks. Standalone module.");
 
     ImGui::EndChild();
 }
