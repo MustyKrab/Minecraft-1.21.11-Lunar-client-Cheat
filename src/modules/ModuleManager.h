081f4ee21d13dcdf768d8bd399006f89fb7daa33
MustyKrab <jarrodmorris5@gmail.com>
2026-04-30 19:10:08 -0400

    fix(ModuleManager): add Cleanup method declaration

--- src/modules/ModuleManager.h
+++ src/modules/ModuleManager.h
@@ -8,6 +8,7 @@ public:
     static std::vector<Module*> modules;
 
     static void Initialize();
+    static void Cleanup();
     static void OnTick();
     static Module* GetModule(const std::string& name);
     static const std::vector<Module*>& GetModules() { return modules; }
