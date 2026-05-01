d3dd650009ac863792eb49afedcb5db19ba217a6
MustyKrab <jarrodmorris5@gmail.com>
2026-04-30 21:18:20 -0400

    feat(Macro): add Macro.h header

---
+++ src/modules/combat/Macro.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "../Module.h"
+#include <random>
+
+class Macro : public Module {
+private:
+    bool stunSlamEnabled = false;
+    bool spearDashEnabled = false;
+    
+    // State machine for StunSlam
+    int stunSlamState = 0;
+    long long stunSlamNextTime = 0;
+    
+    // State machine for SpearDash
+    int spearDashState = 0;
+    long long spearDashNextTime = 0;
+
+    int GaussianSleep(double mean, double stddev, int min_val, int max_val);
+    void SendKeyDownEx(WORD vKey);
+    void SendKeyUpEx(WORD vKey);
+    void SendMouseDownEx();
+    void SendMouseUpEx();
+    long long GetTimeMs();
+
+public:
+    Macro();
+    void OnTick() override;
+    
+    bool IsStunSlamEnabled() const { return stunSlamEnabled; }
+    void SetStunSlamEnabled(bool e) { stunSlamEnabled = e; }
+    
+    bool IsSpearDashEnabled() const { return spearDashEnabled; }
+    void SetSpearDashEnabled(bool e) { spearDashEnabled = e; }
+};
