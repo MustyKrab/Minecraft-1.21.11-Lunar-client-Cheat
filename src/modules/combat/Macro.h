eac198ec061c2f703514c7583e4006a41c192afd
MustyKrab <jarrodmorris5@gmail.com>
2026-04-30 22:13:50 -0400

    Update Macro.h with dynamic hotbar slot tracking

--- src/modules/combat/Macro.h
+++ src/modules/combat/Macro.h
@@ -1,20 +1,19 @@
 #pragma once
 #include "../Module.h"
 #include <random>
-#include <windows.h> // FIX: Added to define WORD
+#include <windows.h>
 
 class Macro : public Module {
 private:
-    bool stunSlamEnabled = false;
-    bool spearDashEnabled = false;
-    
-    // State machine for StunSlam
     int stunSlamState = 0;
     long long stunSlamNextTime = 0;
+    char currentAxeKey = '1';
+    char currentMaceKey = '9';
     
-    // State machine for SpearDash
     int spearDashState = 0;
     long long spearDashNextTime = 0;
+    char currentNoCdKey = '1';
+    char currentSpearKey = '2';
 
     int GaussianSleep(double mean, double stddev, int min_val, int max_val);
     void SendKeyDownEx(WORD vKey);
@@ -22,14 +21,10 @@ private:
     void SendMouseDownEx();
     void SendMouseUpEx();
     long long GetTimeMs();
+    
+    int FindHotbarSlot(void* env_ptr, void* inventory_ptr, const char* itemKey, const char* enchKey);
 
 public:
     Macro();
     void OnTick() override;
-    
-    bool IsStunSlamEnabled() const { return stunSlamEnabled; }
-    void SetStunSlamEnabled(bool e) { stunSlamEnabled = e; }
-    
-    bool IsSpearDashEnabled() const { return spearDashEnabled; }
-    void SetSpearDashEnabled(bool e) { spearDashEnabled = e; }
 };
